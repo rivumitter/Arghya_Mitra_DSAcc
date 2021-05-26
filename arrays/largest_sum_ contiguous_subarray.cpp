7f7c423b485dd8accc4fdb2f0556ac381389b566
rivumitter <rivumitter@gmail.com>
2021-05-26 14:54:09 +0530

    largest_sum_ contiguous_subarray

---
+++ arrays/largest_sum_ contiguous_subarray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int maxSum = INT_MIN;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxSum = max(maxSum,sum);
+            sum = max(0,sum);
+        }
+        return maxSum;
+    }
+int main() {
+	// your code goes here
+	return 0;
+}
