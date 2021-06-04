618f31a632a5767bff9d5ab5d79751ad73be6171
rivumitter <rivumitter@gmail.com>
2021-06-05 00:12:13 +0530

    kth_largest

---
+++ arrays/kth_largest.cpp
@@ -0,0 +1,30 @@
+/*
+    submission link : https://leetcode.com/submissions/detail/503036356/
+*/
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        priority_queue <int, vector<int>, less<int> > pq;
+        for(int i=0;i<n;i++)
+            pq.push(nums[i]);
+        
+        int count = 1,ans = -1;
+        while(!pq.empty()){
+            if(count==k)
+            {
+                ans = pq.top();
+                break;
+            }
+            count++;
+            pq.pop();
+        }
+        return ans;
+    }
+int main() {
+	// your code goes here
+	return 0;
+}
